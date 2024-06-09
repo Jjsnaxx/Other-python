47377dbafac86db4597af8af0f7ce958f463bca0
JYC <140260457+Jjsnaxx@users.noreply.github.com>
2024-06-09 22:34:50 +1000

    Create split-demo.py

---
+++ split-demo.py
@@ -0,0 +1,41 @@
+string1 = "I am a list"
+
+
+
+print(string1.split(","))
+
+print(string1.rjust(15,"X"))
+
+
+
+part1 = "I am"
+
+part2 = " a string"
+
+
+
+print(part1 + part2)
+
+
+
+print("This string is " + str(len(part1+part2)) + " characters long")
+
+
+
+String10 = "I am a list"
+
+
+
+print("String10 is {} characters long!".format(len(String10)))
+
+
+
+print( "{2} {0} {1}".format(len(String10), 5.1, 0x12))
+
+
+
+length = len(String10)
+
+
+
+print(f"String 10 is {length} characters long!")
