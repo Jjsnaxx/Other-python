610bb0b6e93592ef2a05a09ccc08c622236ea77a
JYC <140260457+Jjsnaxx@users.noreply.github.com>
2024-06-09 22:33:08 +1000

    Create dictionaries-demo.py

---
+++ dictionaries-demo.py
@@ -0,0 +1,58 @@
+Dict1= {"Jay": 1, "Bob": 2, "James": 3}
+
+
+
+print(Dict1)
+
+print(type(Dict1))
+
+print(len(Dict1))
+
+print(Dict1["Jay"])
+
+
+
+Dict2= {1:"Jay", 2:"Bob", 3:"James"}
+
+
+
+print(Dict2[1])
+
+
+
+print(Dict2.get(1))
+
+
+
+Dict2[4] = "Ted"
+
+
+
+print(Dict2)
+
+
+
+
+
+Dict2[1] = "Todd"
+
+
+
+print(Dict2[1])
+
+
+
+print("---------------------------------")
+
+
+
+Dict2= {1:"Jay", 2:"Bob", 3:"James"}
+
+
+
+Dict2 [3] = {4: "Jordan", 5: "Ted"}
+
+
+
+print(Dict2)
+
